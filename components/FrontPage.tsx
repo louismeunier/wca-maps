625ade832bc5435124dd691ccc5bfe2cb7e4e05c
louismeunier <louismeunier123@gmail.com>
2021-04-10 21:19:40 -0400

    UI and logic updates

--- components/FrontPage.tsx
+++ components/FrontPage.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { getUserCompetitions, searchUsers } from "@logic/wca-api";
 import { averageGeoPoint } from "@logic/calculations";
 import type { LatLngExpression } from "leaflet";
+//import { LocationStats } from "@components/LocationStats";
+import { Loading } from "./Loading";
 
 const MapLayer = dynamic(() => import("@components/MapLayer"), {
     ssr: false
@@ -13,14 +15,20 @@ const MapWithNoSSR = dynamic(() => import("@components/Map"), {
 });
 
 export default function FrontPage():JSX.Element {
-    const [layers, setLayers] = useState<JSX.Element[]>([]);
-    const [wcaID, setWcaID] = useState<string|null>(null);
-    const [autocomplete, setAutocomplete] = useState<WCAUser[]|null>(null);
+    //utils
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [autocomplete, setAutocomplete] = useState<WCAUser[]|null>(null);
+    //passed to the map
+    const [layers, setLayers] = useState<JSX.Element[]>([]);
     const [center, setCenter] = useState<LatLngExpression>([0,0])
+    //used for stats
+    const [wcaData, setWcaData] = useState<WCAUser|null>(null);
+    const [competitionData, setCompetitionData] = useState<WCACompetition[]|null>(null);
 
-    const handleChange = (e: { target: { value: any; }; }) => {
-        searchUsers(e.target.value)
+    const handleChange = (e:React.ChangeEvent<HTMLInputElement>)  => {
+        e.target.value === "" 
+        ? setAutocomplete(null)
+        : searchUsers(e.target.value)
             .then(res => {
                 const filteredUsers:WCAUser[] = res.result.filter(user => user.wca_id).slice(0,5);
                 setAutocomplete(filteredUsers);
@@ -31,61 +39,102 @@ export default function FrontPage():JSX.Element {
     }
     
     const handleClick = () => {
-        if (wcaID) {
-            setIsLoading(true)
-            getUserCompetitions(wcaID)
-                .then(res => {
-                    Promise.all(res)
-                        .then(resArr => {
-                            const layers:JSX.Element[] = [];
-                            const coordinateArray:number[][] = resArr.filter(competition => {
-                                if (competition.city.toLowerCase() === 'multiple cities') return false
-                                else return true
-                            }).map((competition, index)=> {
-                                layers.push(<MapLayer key={index+1} position={[competition.latitude_degrees, competition.longitude_degrees]} message={`${competition.name}`}/>)
-                                return [competition.latitude_degrees, competition.longitude_degrees]
-                                })
+        if (!wcaData || !wcaData?.wca_id) return;
+        setIsLoading(true)
+        getUserCompetitions(wcaData.wca_id)
+            .then(res => {
+                Promise.all(res)
+                    .then(resArr => {
+                        const layers:JSX.Element[] = [];
+                        const filteredCompetitions:WCACompetition[] = resArr.filter(competition => {
+                            if (competition.city.toLowerCase() === 'multiple cities') return false
+                            else return true
+                        });
 
-                            const averageCoordinate:LatLngExpression = averageGeoPoint(coordinateArray)
-                            setCenter(averageCoordinate)
-                            setLayers([<MapLayer notable key={0} position={averageCoordinate} message={`Average`}/>, ...layers])
-                            setIsLoading(false)
-                        })
-                })
-                .catch(err => console.error(err))
-        }
+                        const coordinateArray:number[][] = filteredCompetitions.map((competition, index)=> {
+                            layers.push(<MapLayer key={index+1} position={[competition.latitude_degrees, competition.longitude_degrees]} message={`${competition.name}`}/>)
+                            return [competition.latitude_degrees, competition.longitude_degrees]
+                        });
+
+                        setCompetitionData(filteredCompetitions);
+
+                        const averageCoordinate:LatLngExpression = averageGeoPoint(coordinateArray)
+
+                        setCenter(averageCoordinate)
+                        setLayers([<MapLayer notable key={0} position={averageCoordinate} message={`Average`}/>, ...layers])
+                        setIsLoading(false)
+                    })
+            })
+            .catch(err => console.error(err))
     }
 
     useEffect(() => {
-        handleClick()
-    }, [wcaID])
+        handleClick();
+    }, [wcaData])
+
+    useEffect(() => {
+        setAutocomplete(null)
+    }, [competitionData])
 
     return (
-        <div className="w-full h-full m-0 grid grid-cols-1">
-                <div className="absolute z-50 h-full left-0 w-1/3 border-gray-800 bg-gray-200 border-2 bg-opacity-70 rounded-lg grid place-items-center">
-                    <div className="h-1/3 grid place-items-center">
-                        <h1>Search for a user</h1>
-                        {isLoading && <h1 className="text-red-500 absolute italic">Loading...</h1>}
-                        <div>
-                            <input className="rounded-lg text-center" placeholder="Begin typing..." onChange={handleChange} type="text"></input>
-                            <div className="absolute">
-                                {autocomplete && autocomplete.map((user, index) => {
-                                    return (
-                                        <h2 
-                                            className="text-gray-500 hover: cursor-pointer"
-                                            onClick={() => {
-                                                setWcaID(user.wca_id);
-                                            }} 
-                                            key={index}>
-                                                {user.name} <span className="text-sm italic">({user.wca_id})</span>
-                                        </h2>
-                                    )
-                                })}
+        <>
+        {/* Could make this a portal? */}
+            {
+                isLoading && (
+                <div className="absolute top-1/2 left-1/2 z-50">
+                    <Loading/>
+                </div>
+                )
+            }
+
+            <div className="w-screen h-screen m-0">
+                <div className="fixed w-1/3 h-full left-0 z-50 bg-gray-400 border-gray-600 border-2 rounded-r-xl bg-opacity-75">
+                    <h1 className="top-0 underline text-blue-400 text-4xl absolute w-full text-center">WCA Map Stats</h1>
+                    <div className="grid place-items-center h-full">
+                        <div className="w-2/3 h-1/3 flex flex-col content-center">
+                            {/* I'm pretty sure this logic will break when built; I've had that problem before */}
+                            <input 
+                                className={`w-full p-2 text-center ${autocomplete ? "rounded-t-xl" : "rounded-xl"}`} 
+                                placeholder="Start typing a name or WCA ID" 
+                                onChange={ handleChange } 
+                                type="text"/>
+                            <div className="sticky text-center bg-gray-200 rounded-b-xl">
+                                {
+                                    autocomplete && autocomplete.map((user, key) => {
+                                        return (
+                                            <div 
+                                                onClick={() => setWcaData(user)}
+                                                className="hover: cursor-pointer p-2 text-center border-t-2 border-gray-500" 
+                                                key={key}
+                                                >
+                                                <span className="underline text-lg">
+                                                    {user.name}
+                                                    <span className="no-underline text-sm">
+                                                        ({user.wca_id})
+                                                    </span>
+                                                </span>
+                                            </div>
+                                        )
+                                    })
+                                }
                             </div>
                         </div>
                     </div>
+                    <div className="absolute text-center w-full bottom-0">
+                        <h1 className="">
+                            made by <a 
+                                className="underline text-blue-400"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://github.com/louismeunier"
+                            >
+                                 louismeunier
+                            </a>
+                        </h1>
+                    </div>
                 </div>
                 <MapWithNoSSR center={center} layers={layers}/>
-        </div>
+            </div>
+        </>
     )
 }
